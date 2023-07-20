1cf36a203efb69fb66b6a7fa153141606cc996fc
Inna-Kulish <norelajest@gmail.com>
2023-07-20 23:05:16 +0300

    Local Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,28 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { MainContainer, Title } from 'components/App/App.styled';
 
+const LS_KEY = 'phonebook_contacts';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(LS_KEY);
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = data => {
     const { name, number } = data;
@@ -33,7 +45,7 @@ class App extends Component {
   };
 
   deleteContact = contactId => {
-  this.setState(prevState => ({
+    this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
